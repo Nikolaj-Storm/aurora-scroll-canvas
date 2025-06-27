d5d63dacfdc1e60b51d7c3aa9483350c07e09fda
Nikolaj-Storm <121698062+Nikolaj-Storm@users.noreply.github.com>
2025-06-27 21:02:47 +0200

    Update portfolioConfig.ts

--- src/config/portfolioConfig.ts
+++ src/config/portfolioConfig.ts
@@ -6,7 +6,7 @@ export const portfolioConfig = {
     lastName: "Storm Petersen",
     tagline: "",
     profileImage: "/lovable-uploads/a4e71c80-eb0f-4ad6-91cb-b174fd7f4db3.png",
-    floatingCircleImage: "/Small.png"
+    floatingCircleImage: "/assets/Small.png"
   },
 
   // About Section
